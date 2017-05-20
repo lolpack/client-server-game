4901426a9bb4df3e488114254c963110a8eff148
Aaron Pollack <pollackaaron@gmail.com>
2017-05-20 12:49:35 -0700

    Create initial server file

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # client-server-game
 Multi Threaded Client Server Game in C++
+
+`g++ -std=c++11 server.cpp -lpthread -o server`
---
+++ server.cpp
@@ -0,0 +1,35 @@
+#include <sys/types.h> // size_t, ssize_t
+#include <sys/socket.h> // socket funcs
+#include <netinet/in.h> // sockaddr_in
+#include <arpa/inet.h> // htons, inet_pton
+#include <unistd.h> // close
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <pthread.h>
+#include <semaphore.h>
+#include <vector>
+
+using namespace std;
+
+int main () {
+  unsigned short servPort = 4567;
+  struct sockaddr_in servAddr;
+  const int MAXPENDING = 5;
+
+  servAddr.sin_family = AF_INET;  // always AF_INET
+  servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+  servAddr.sin_port = htons(servPort);
+
+  int sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP); if (sock < 0) {
+    cerr << "Error with socket" << endl; exit (-1);
+  }
+
+  if (bind(sock, (struct sockaddr *) &servAddr, sizeof(servAddr)) < 0) {
+    cerr << "Error with bind" << endl; exit (-1);
+  }
+
+  if (listen(sock, MAXPENDING) < 0 ) { //Listen is non‐blocking: returns immediately.
+    cerr << "Error with listen" << endl; exit (-1);
+  }
+}
