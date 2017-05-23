347ed9d4aa06af59c8a9917c75bc6d8c9df1a8ee
Aaron Pollack <pollackaaron@gmail.com>
2017-05-22 21:31:33 -0700

    AFA

--- server.cpp
+++ server.cpp
@@ -169,7 +169,7 @@ void* receiveRequest(void *arg) {
 
   for (int j = 0; j < topThree; j++) {
     Winner tempwin = leaderBoard->top();
-    leaderBoard->pop();
+    // leaderBoard->pop();
     string eachRow = string(to_string(j + 1)) + string(". ") + string(tempwin.name) + string(" ") + string(to_string(tempwin.turns)) + string("&&");
 
     leaderBoardText = leaderBoardText + eachRow;
@@ -183,6 +183,8 @@ void* receiveRequest(void *arg) {
     leaderBoard->push(tempLeaderBoard.back()); // Take items temporarily in pQueue and put it back in vector;
     tempLeaderBoard.pop_back();
   }
+
+  cout << "LEADER BOARD SIZE" << leaderBoard->size() << endl;
 
   sem_post(&leaderBoardLock);
 
